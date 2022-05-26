54806f752c1050fae22ffa7391150b8f05364e61
Antonio <antoniogallegos086@gmail.com>
2022-05-26 00:26:09 -0500

    First steps

---
+++ user.py
@@ -0,0 +1,34 @@
+class User:
+    id = ''
+    nombre = ""
+    apellidos = ""
+    tipo_usuario = ""
+    visible = True
+
+    def __init__(self):
+        print('Se ejecuto el metodo constructor')
+
+    def listarTodo(self):
+        print('listarTodoaqui')
+
+
+    def crear(self):
+        print('hola')
+
+    def buscar_texto(self):
+        print('algo')
+
+    def buscar_id(self):
+        print('id')
+
+    def actualizar(self):
+        print('id')
+
+    def eliminar(self):
+        print('eliminar')
+
+
+
+
+
+
